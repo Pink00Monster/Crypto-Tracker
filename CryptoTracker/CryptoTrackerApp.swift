3c1a24ef2c6b7fc7d705285d23689836453218c2
Pink00Monster <vanillaWangyi@163.com>
2023-05-31 11:53:53 +0200

    Initialise the project

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by Vanilla on 31/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
